7c572f28a1b3136d381bea97c040919998746812
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-03-12 15:52:07 +0700

    feat: events

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 const categoriesRouter = require('./app/api/v1/categories/router');
 const imagesRouter = require("./app/api/v1/images/router");
 const talentsRouter = require("./app/api/v1/talents/router");
+const eventsRouter = require("./app/api/v1/events/router");
 
 
 const notFoundMiddleware = require('./app/middlewares/not-found');
@@ -32,6 +33,7 @@ app.get('/', (req, res) => {
 app.use(v1, categoriesRouter);
 app.use(v1, imagesRouter);
 app.use(v1, talentsRouter);
+app.use(v1, eventsRouter);
 
 // middlewares
 app.use(notFoundMiddleware);
